b9f1bc6d6ea0edea99883203e9dd0f83e11efd6d
George Steel <george@net-glue.co.uk>
2019-12-17 19:37:52 +0000

    Initial setup

---
+++ src/Something.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+class Something
+{
+    /** @var int */
+    private $something;
+
+    public function __construct(int $something)
+    {
+        $this->something = $something;
+    }
+
+    public function something() : int
+    {
+        return $this->something;
+    }
+}
---
+++ test/SomethingTest.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test;
+
+use App\Something;
+use PHPUnit\Framework\TestCase;
+
+class SomethingTest extends TestCase
+{
+    public function testSomething() : void
+    {
+        $something = new Something(10);
+        $this->assertSame(10, $something->something());
+    }
+}
